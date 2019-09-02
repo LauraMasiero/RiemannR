58516be606d7ce7fbdd15bb5b98569400f1c9635
Laura Masiero <36959927+LauraMasiero@users.noreply.github.com>
2019-09-02 09:52:19 +0200

    Update Functions.R

--- R/Functions.R
+++ R/Functions.R
@@ -78,14 +78,12 @@ log_map <- function(scm_list, C){
 }
 
 #####EXPONENTIAL MAPPING#####
-exp_map <- function(mapped_values, C){
   require(powerplus)
   require(expm)
   C_star <- Matpow(C, 0.5,1)
   C_2star <- Matpow(C, -0.5,1)
   orig_space_values <- list()
-  for(i in 1:length(mapped_values))
-    orig_space_values[[i]] <- C_star%*%expm(C_2star%*%mapped_values[[i]]%*%C_2star)%*%C_star
+    orig_space_values[[1]] <- C_star%*%expm(C_2star%*%mapped_values%*%C_2star)%*%C_star
   return(orig_space_values)
 }
 
