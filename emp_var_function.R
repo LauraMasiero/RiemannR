0d32a49ca07d021276d7adfb170ca5c847b30026
Laura Masiero <36959927+LauraMasiero@users.noreply.github.com>
2020-09-18 10:57:15 +0200

    Delete emp_var_function.R

--- emp_var_function.R
+++
@@ -1,9 +0,0 @@
-emp_var_function <- function(mu, scm_list) {
-  P <- length(scm_list)
-  P^(-1)*Reduce("+",lapply(scm_list, function(x) RaoDist(x, mu)))
-}
-
-eta <- function(sigma) {
-  -1/(2*sigma^2)
-} 
-
